92de9599bed4b842e9fc2e5af4967f2e7c6bd1a0
Jin <lochjin@gmail.com>
2021-06-16 15:56:11 +0800

    debug payledger

--- cmd/payledger/binode.go
+++ cmd/payledger/binode.go
@@ -82,47 +82,109 @@ func (node *BINode) DB() database.DB {
 }
 
 func (node *BINode) statistics() error {
-	total := node.bc.BlockDAG().GetBlockTotal()
+	bd := node.bc.BlockDAG()
+
+	endID := uint(0)
+
+	// 1:find end
+	mainTip := bd.GetMainChainTip()
+	for cur := mainTip; cur != nil; cur = node.bc.BlockDAG().GetBlockById(cur.GetMainParent()) {
+		block, err := node.bc.FetchBlockByHash(cur.GetHash())
+		if err != nil {
+			return err
+		}
+		if block.Transactions()[0].Tx.TxOut[0].Amount != 0 {
+			endID = cur.GetID()
+			break
+		}
+	}
+
+	if endID == 0 {
+		return fmt.Errorf("End block id error\n")
+	}
+
+	total := endID + 1
 	validCount := 1
 	subsidyCount := 0
 	subsidy := uint64(0)
-	fmt.Printf("Process...   ")
+
+	// all blue blocks
+	mainTip = bd.GetBlockById(endID)
+	log.Info(fmt.Sprintf("Find mining main tip:id=%d hash=%s", endID, mainTip.GetHash()))
+
+	bluesArr := make([]bool, total)
+
+	for cur := mainTip; cur != nil; cur = node.bc.BlockDAG().GetBlockById(cur.GetMainParent()) {
+		bluesArr[cur.GetID()] = true
+		for k := range cur.(*blockdag.PhantomBlock).GetBlueDiffAnticone().GetMap() {
+			bluesArr[k] = true
+		}
+	}
+
+	// genesis
+	genblock, err := node.bc.FetchBlockByHash(bd.GetGenesisHash())
+	if err != nil {
+		return err
+	}
+	gencoinbaseAmout := uint64(0)
+	for _, v := range genblock.Transactions()[0].Tx.TxOut {
+		gencoinbaseAmout += v.Amount
+	}
+	fmt.Printf("id:%8d hash:%s txsvalid:%t coinbaseAmout:%18d coinbaseValid:%t isBlue:%t\n", 0, bd.GetGenesisHash(), true, gencoinbaseAmout, true, true)
+
+	coinbaseValidNum := 1
+
 	for i := uint(1); i < total; i++ {
 		ib := node.bc.BlockDAG().GetBlockById(i)
 		if ib == nil {
-			return fmt.Errorf("No block:%d", i)
+			return fmt.Errorf("No block:%d\n", i)
 		}
+		if !ib.IsOrdered() {
+			return fmt.Errorf("Block is not ordered:%s (%d)\n", ib.GetHash(), ib.GetID())
+		}
+		block, err := node.bc.FetchBlockByHash(ib.GetHash())
+		if err != nil {
+			return err
+		}
+
+		coinbaseAmout := block.Transactions()[0].Tx.TxOut[0].Amount
+		coinbaseValid := false
+
 		if !knownInvalid(byte(ib.GetStatus())) {
 			validCount++
-
-			block, err := node.bc.FetchBlockByHash(ib.GetHash())
-			if err != nil {
-				return err
-			}
-
-			txfullHash := block.Transactions()[0].Tx.TxHashFull()
-
-			if isTxValid(node.db, block.Transactions()[0].Hash(), &txfullHash, ib.GetHash()) {
-				if node.bc.BlockDAG().IsBlue(i) {
-					subsidyCount++
-					subsidy += block.Transactions()[0].Tx.TxOut[0].Amount
-				}
-			}
 		}
 
+		txfullHash := block.Transactions()[0].Tx.TxHashFull()
+
+		if isTxValid(node.db, block.Transactions()[0].Hash(), &txfullHash, ib.GetHash()) {
+			if bluesArr[i] {
+				subsidyCount++
+				subsidy += block.Transactions()[0].Tx.TxOut[0].Amount
+
+			}
+			coinbaseValid = true
+		}
+
+		if coinbaseValid {
+			coinbaseValidNum++
+		}
+		fmt.Printf("id:%8d hash:%s txsvalid:%t coinbaseAmout:%18d coinbaseValid:%t isBlue:%t\n", i, ib.GetHash(), !knownInvalid(byte(ib.GetStatus())), coinbaseAmout, coinbaseValid, bluesArr[i])
 	}
-	mainTip := node.bc.BlockDAG().GetMainChainTip().(*blockdag.PhantomBlock)
-	blues := mainTip.GetBlueNum() + 1
+
+	blues := mainTip.(*blockdag.PhantomBlock).GetBlueNum() + 1
 	reds := mainTip.GetOrder() + 1 - blues
 	unconfirmed := total - (mainTip.GetOrder() + 1)
 
 	fmt.Println()
-	fmt.Printf("Total:%d   Valid:%d   BlueNum:%d   RedNum:%d   SubsidyNum:%d Subsidy:%d", total, validCount, blues, reds, subsidyCount, subsidy)
+
+	resultStr := fmt.Sprintf("数据库分析:(包含创世区块; 主链Tip Hash: %s (%d)\n总区块数:%d\n合法的区块数:%d\nCoinbase数据库记录数:%d\n包含重复Coinbase交易的区块数:%d\n红色块数:%d\n蓝色块数:%d\n合法蓝色区块:%d (不包括创世块)\n铸币量:%d * 12000000000 = %d\n流通量:%d + %d = %d\n",
+		mainTip.GetHash(), endID, total, validCount, coinbaseValidNum, validCount-coinbaseValidNum, reds, blues, subsidyCount, subsidyCount, subsidy, gencoinbaseAmout, subsidy, gencoinbaseAmout+subsidy)
+
 	if unconfirmed > 0 {
-		fmt.Printf(" Unconfirmed:%d", unconfirmed)
+		resultStr = fmt.Sprintf("%s Unconfirmed:%d", resultStr, unconfirmed)
 	}
-	fmt.Println()
-	fmt.Println("(Note:SubsidyNum does not include genesis.)")
+	fmt.Println(resultStr)
+	log.Info(resultStr)
 
 	return nil
 }
